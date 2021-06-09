16dbcbda033b10f092da561cde97b9555679d898
Varna Hernandez <varna.hernandez@visitor.upm.com>
2021-06-08 23:04:20 -0300

    Set connection string to ShoppingDBContext

--- vm-shopping-api/vm-shopping-api/Startup.cs
+++ vm-shopping-api/vm-shopping-api/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -10,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using vm_shopping_data_access;
 
 namespace vm_shopping_api
 {
@@ -26,6 +28,8 @@ namespace vm_shopping_api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<ShoppingDBContext>(options =>
+                                                     options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
