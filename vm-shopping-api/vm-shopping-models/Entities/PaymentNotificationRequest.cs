a627a8c151d5a87a82ce80ee53bd596da6cccfb5
Varna Hernandez <varna.hernandez@visitor.upm.com>
2021-06-12 14:33:34 -0300

    Add some entities related with Payment Notifications

---
+++ vm-shopping-api/vm-shopping-models/Entities/PaymentNotificationRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vm_shopping_models.Entities
+{
+    public class PaymentNotificationRequest
+    {
+        public StatusNotificationRequest status { get; set; }
+        public int requestId { get; set; }
+        public string reference { get; set; }
+        public string signature { get; set; }
+    }
+}
