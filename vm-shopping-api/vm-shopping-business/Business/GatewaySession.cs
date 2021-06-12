fddc1f7f79f54894a06d6d0d268db5234cb52791
Varna Hernandez <varna.hernandez@visitor.upm.com>
2021-06-11 23:53:12 -0300

    Add GatewaySession class

---
+++ vm-shopping-api/vm-shopping-business/Business/GatewaySession.cs
@@ -0,0 +1,23 @@
+﻿using PlacetoPay.Integrations.Library.CSharp.Contracts;
+using System;
+using vm_shopping_business.Interfaces;
+
+namespace vm_shopping_business.Business
+{
+    public class GatewaySession : IGatewaySession
+    {
+        PlacetoPay.Integrations.Library.CSharp.PlacetoPay shoppingGatewaySession;
+        public PlacetoPay.Integrations.Library.CSharp.PlacetoPay Session()
+        {
+            if (shoppingGatewaySession == null)
+            {
+                //ToDo: Take these data from Configuration
+                shoppingGatewaySession = new PlacetoPay.Integrations.Library.CSharp.PlacetoPay("6dd490faf9cb87a9862245da41170ff2",
+                                      "024h1IlD",
+                                      new Uri("https://test.placetopay.com/redirection/"),
+                                      Gateway.TP_REST);
+            }
+            return shoppingGatewaySession;
+        }
+    }
+}
---
+++ vm-shopping-api/vm-shopping-business/Interfaces/IGatewaySession.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using P2P = PlacetoPay.Integrations.Library.CSharp.PlacetoPay;
+
+namespace vm_shopping_business.Interfaces
+{
+   public interface IGatewaySession
+    {
+        P2P Session();
+    }
+}
