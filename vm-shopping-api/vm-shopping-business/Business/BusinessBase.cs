fef20609b26a685fd95f7f327ab91d38f24b039b
Varna Hernandez <varna.hernandez@visitor.upm.com>
2021-06-11 23:51:15 -0300

    Add BusinessBase class

---
+++ vm-shopping-api/vm-shopping-business/Business/BusinessBase.cs
@@ -0,0 +1,14 @@
+﻿using vm_shopping_data_access;
+
+namespace vm_shopping_business.Business
+{
+    public class BusinessBase
+    {
+        public readonly ShoppingDBContext shoppingDBContext;
+
+        public BusinessBase(ShoppingDBContext shoppingDBContext)
+        {
+            this.shoppingDBContext = shoppingDBContext;
+        }
+    }
+}
