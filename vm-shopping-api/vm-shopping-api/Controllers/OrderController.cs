9f3c39dadb0688947fa603e2e87c09ad974afe3b
Varna Hernandez <varna.hernandez@visitor.upm.com>
2021-06-11 23:58:16 -0300

    Include create request method

--- vm-shopping-api/vm-shopping-api/Controllers/OrderController.cs
+++ vm-shopping-api/vm-shopping-api/Controllers/OrderController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
+using vm_shopping_business.Interfaces;
+using vm_shopping_models.Entities;
 
 namespace vm_shopping_api.Controllers
 {
@@ -7,11 +10,32 @@ namespace vm_shopping_api.Controllers
     [Route("api/[controller]")]   
     public class OrderController : ControllerBase
     {
+        public readonly IOrderBusiness orderBusiness;
+
+        public OrderController(IOrderBusiness orderBusiness)
+        {
+            this.orderBusiness = orderBusiness;
+        }
 
         [HttpGet]
         public async Task<ActionResult> Get()
         {
-            return Ok("Welcome to vm Shopping");
+            return Ok("VM Shopping running");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody]OrderRequest orderRequest)
+        {           
+            try
+            {
+                OrderResponse orderResponse = await orderBusiness.CreateOrderAsync(orderRequest);
+                return Ok(orderResponse);
+            }
+            catch (Exception ex)
+            {
+                //ToDo: Logg
+                return StatusCode(500);
+            }
         }
     }
 }
