c29ab531fb37c6d96a021b1985931c53c7aaa5c5
Varna Hernandez <varna.hernandez@visitor.upm.com>
2021-06-12 14:31:28 -0300

    Add PaymentNotification table

---
+++ vm-shopping-api/vm-shopping-data-access/Entities/PaymentNotification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vm_shopping_data_access.Entities
+{
+    public class PaymentNotification
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public string Message { get; set; }
+        public string Reason { get; set; }
+        public string Reference { get; set; }
+        public string Signature { get; set; }
+        public DateTime Date { get; set; }
+        public Order Order { get; set; }
+    }
+}
