7b216e1479b3d8df447f308923714acbef8fd74c
Varna Hernandez <varna.hernandez@visitor.upm.com>
2021-06-12 01:54:34 -0300

    Create StatusResponse class

---
+++ vm-shopping-api/vm-shopping-models/Entities/StatusResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vm_shopping_models.Entities
+{
+    public class StatusResponse
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+    }
+}
