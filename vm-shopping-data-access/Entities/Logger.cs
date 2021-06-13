8bd2e28d99f4871b86f3e0230114fae536f9a125
varnamartha <varnamartha@gmail.com>
2021-06-12 22:02:14 -0300

    Add Logger entity

---
+++ vm-shopping-data-access/Entities/Logger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vm_shopping_data_access.Entities
+{
+    public class Logger
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public string Details { get; set; }
+        public string LogLevel { get; set; }
+        public DateTime LogDate { get; set; }
+    }
+}
