2dd41416c5de4bc6716147ff634fd55936872c66
varnamartha <varnamartha@gmail.com>
2021-06-13 15:53:14 -0300

    Create get order by id test

---
+++ vm-shopping-test/Queries/OrderQuery.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using vm_shopping_business.Interfaces;
+using vm_shopping_data_access;
+using vm_shopping_models.Entities;
+
+namespace vm_shopping_test.DataBase
+{
+    public class OrderQuery
+    {
+        private readonly ShoppingDBContext context;
+
+        public readonly IOrderBusiness orderBusiness;
+
+        public OrderQuery(IOrderBusiness orderBusiness, ShoppingDBContext context)
+        {
+            this.context = context;
+            this.orderBusiness = orderBusiness;
+        }      
+
+        public Task<OrderResponse> GetOrderByIdExecute(int orderId)
+        {
+            return orderBusiness.GetOrder(orderId);
+
+        }
+    }
+}
---
+++ vm-shopping-test/Test/OrderTest.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using vm_shopping_business;
+using vm_shopping_business.AutoMapper;
+using vm_shopping_business.Business;
+using vm_shopping_business.Interfaces;
+using vm_shopping_data_access;
+using vm_shopping_test.DataBase;
+using vs_shopping_test.DataBase;
+
+namespace vm_shopping_test.Test
+{
+    public class OrderTest
+    {
+        private IAutoMapperConfig autoMapperConfig;
+
+        private IGatewaySession gatewaySession;
+
+        private IClientBusiness clientBusiness;
+
+        private IProductBusiness productBusiness;
+
+        private IOrderBusiness orderBusiness;
+
+        private void InitializeBusinessClass(ShoppingDBContext context)
+        {
+            if (autoMapperConfig == null)
+            {
+                autoMapperConfig = new AutoMapperConfig();
+                gatewaySession = new GatewaySession();
+
+                clientBusiness = new ClientBusiness(autoMapperConfig, context);
+
+                productBusiness = new ProductBusiness(autoMapperConfig, context);
+
+                orderBusiness = new OrderBusiness(gatewaySession, clientBusiness, productBusiness, autoMapperConfig, context);
+            }
+        }
+
+        [Test]
+        public void GetOrderById()
+        {
+            var context = Scheme.CreateContextMock();
+            InitializeBusinessClass(context);
+
+            var query = new OrderQuery(orderBusiness, context);
+
+            var result = query.GetOrderByIdExecute(5);
+
+            Assert.AreEqual(5, result.Result.ShoppingOrderId);
+        }
+    }
+}
