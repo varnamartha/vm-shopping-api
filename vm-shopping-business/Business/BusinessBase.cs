9d0cb19a0abaa828780af7353f551418fb245a87
varnamartha <varnamartha@gmail.com>
2021-06-12 22:06:45 -0300

    Add method to create log info and error

--- vm-shopping-business/Business/BusinessBase.cs
+++ vm-shopping-business/Business/BusinessBase.cs
@@ -1,4 +1,7 @@
-﻿using vm_shopping_data_access;
+﻿using Microsoft.Extensions.Logging;
+using System;
+using vm_shopping_data_access;
+using vm_shopping_data_access.Entities;
 
 namespace vm_shopping_business.Business
 {
@@ -9,6 +12,32 @@ namespace vm_shopping_business.Business
         public BusinessBase(ShoppingDBContext shoppingDBContext)
         {
             this.shoppingDBContext = shoppingDBContext;
+        }
+
+        public void LogError(string methodName, Exception ex)
+        {
+            var logger = new Logger
+            {
+                LogDate = DateTime.Now,
+                Message = string.Format("{0} : {1}", methodName, ex.Message),
+                LogLevel = LogLevel.Error.ToString(),
+                Details = ex.InnerException != null ? string.Format("InnerException: {0}", ex.InnerException.Message) : string.Empty,
+            };
+            shoppingDBContext.Add(logger);
+            shoppingDBContext.SaveChanges();
+        }
+
+        public void Log(string methodName, string mesagge)
+        {
+            var logger = new Logger
+            {
+                LogDate = DateTime.Now,
+                Message = string.Format("{0} : {1}", methodName, mesagge),
+                LogLevel = LogLevel.Information.ToString(),
+                Details = "ok"
+            };
+            shoppingDBContext.Add(logger);
+            shoppingDBContext.SaveChanges();
         }
     }
 }
