631010277dc546985d33eb5ac1eef485c2e58ea4
varnamartha <varnamartha@gmail.com>
2021-06-13 18:33:16 -0300

    Rename mocked DB context class

--- vm-shopping-test/DataBase/FakeDBContext.cs
+++ vm-shopping-test/DataBase/FakeDBContext.cs
@@ -3,9 +3,19 @@ using vm_shopping_data_access;
 
 namespace vs_shopping_test.DataBase
 {
-    public static class Scheme
+    public static class FakeDBContext
     {
-        public static ShoppingDBContext CreateContextMock()
+        private static ShoppingDBContext context;
+
+        public static ShoppingDBContext GetDBContext()
+        {
+            if(context==null)
+            {
+                context = CreateContextMock();
+            }
+            return context;
+        }
+        private static ShoppingDBContext CreateContextMock()
         {
             var options = new DbContextOptionsBuilder<ShoppingDBContext>()
                 .UseInMemoryDatabase(databaseName: "Status")
@@ -14,8 +24,8 @@ namespace vs_shopping_test.DataBase
                 .Options;
             var context = new ShoppingDBContext(options);
            
-            context = ProductDB.CreateOrderDB(context);
-            context = StatusDB.CreateOrderDB(context);
+            context = ProductDB.CreateProductDB(context);
+            context = StatusDB.CreateStatusDB(context);
             context = OrderDB.CreateOrderDB(context);
             return context;
         }
