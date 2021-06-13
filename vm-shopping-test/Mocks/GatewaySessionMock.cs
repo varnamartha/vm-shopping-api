e148ffbb141ad46cb81552b5314f7094c7e908b3
varnamartha <varnamartha@gmail.com>
2021-06-13 18:30:28 -0300

    Add mock to placeto pay to fake request

---
+++ vm-shopping-test/Mocks/GatewaySessionMock.cs
@@ -0,0 +1,18 @@
+﻿using vm_shopping_business.Interfaces;
+
+namespace vm_shopping_test.Mocks
+{
+    public class GatewaySessionMock : IGatewaySession
+    {
+        public bool DidFailed { get; set; }
+
+        PlacetoPay.Integrations.Library.CSharp.PlacetoPay IGatewaySession.Session()
+        {
+            var placetoPayMock = new PlacetoPayMock
+            {
+                didFailed = DidFailed
+            };
+            return placetoPayMock;
+        }
+    }
+}
